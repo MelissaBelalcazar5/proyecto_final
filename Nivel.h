26a6b69e78a20f7f6190c6780b4c0899163b9e3e
valcastaneda <3valcastaneda@javerianacali.edu.co>
2025-11-14 12:28:12 -0500

    Update Nivel.h

--- Nivel.h
+++ Nivel.h
@@ -2,12 +2,25 @@
 // Created by Melissa Belalcazar on 12/11/25.
 //
 
-#ifndef PROYECTO_FINAL_NIVEL_H
-#define PROYECTO_FINAL_NIVEL_H
+#ifndef NIVEL_H
+#define NIVEL_H
 
+#include <string>
+#include "Jugador.h"
+#include "Enemigo.h"
+using namespace std;
 
 class Nivel {
+private:
+    string nombre;          // Nombre del nivel
+    string descripcion;     // Texto narrativo del lugar
+    Enemigo* enemigo;       // Enemigo presente en este nivel
+
+public:
+    Nivel(string n, string d, Enemigo* e);
+
+    void mostrarIntroduccion();              // Muestra el texto de bienvenida al nivel
+    void iniciarCombate(Jugador& jugador);   // Controla el combate entre jugador y enemigo
 };
 
-
-#endif //PROYECTO_FINAL_NIVEL_H
+#endif
