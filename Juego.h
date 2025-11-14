dceeb0e10925f3b6f0b5e4d78b165500e41c1721
valcastaneda <3valcastaneda@javerianacali.edu.co>
2025-11-14 12:19:38 -0500

    Update Juego.h

--- Juego.h
+++ Juego.h
@@ -2,12 +2,23 @@
 // Created by Melissa Belalcazar on 12/11/25.
 //
 
-#ifndef PROYECTO_FINAL_JUEGO_H
-#define PROYECTO_FINAL_JUEGO_H
+#ifndef JUEGO_H
+#define JUEGO_H
 
+#include <vector>
+#include "Jugador.h"
+#include "Nivel.h"
+using namespace std;
 
 class Juego {
+private:
+    Jugador jugador;
+    vector<Nivel*> niveles;
+
+public:
+    Juego();                      // Constructor que crea todo el juego
+    void iniciar();               // Muestra la introducción y arranca el juego
+    void jugar();                 // Recorre todos los niveles uno por uno
 };
 
-
-#endif //PROYECTO_FINAL_JUEGO_H
+#endif
